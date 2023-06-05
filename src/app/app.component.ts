9e4a0a66b692b999995b632ac5b8b73979b9e449
nicolasgodoy <76171813+nicolasgodoy@users.noreply.github.com>
2023-06-05 12:52:37 -0300

    pusheando sidebar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,23 +3,36 @@ import { AuthService } from './service/auth.service';
 import { Router } from '@angular/router';
 import Swal from 'sweetalert2';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 
-export class AppComponent {
+export class AppComponent{
   title = 'LabNetAppFront';
-  token: string = '';
-  idUser: number;
+  
+  idUser:number
 
   constructor(private _authService: AuthService, private _router: Router){
+
+    const token = this._authService.readToken();
+
+  
+      
+    const Object = this._authService.DecodeJWT(token);
+      
+      this.idUser= this._authService.getValueByKey(Object,'IdUser');
+      console.log('ID log user:' +this.idUser)
+    }
+
+  ngOnInit(): void {
+  }
+  
  
-   }
-
-
-   logout() {
+   
+  logout() {
     Swal.fire({
       title: 'Cierre de sesión',
       text: "Seguro que desea cerrar sesión?",
